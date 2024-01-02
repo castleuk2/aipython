511deb1b81d1629eba468e599afa9b1358442b08
castleuk2 <147896301+castleuk2@users.noreply.github.com>
2024-01-02 11:53:17 +0900

    Add files via upload

---
+++ 240102.py
@@ -0,0 +1,23 @@
+#github 올리기
+# # 입출력
+# a = int(input('당신의 나이는?'))
+
+# print(a, '살 이군요.')
+# print('내년엔', a+1, '살이 되네요')
+# print(type(a))
+
+# # 입출력1
+
+# a = int(input('your age?'))
+# msg = str(a) + 'is your age'
+# print(msg)
+# print('next year', a+1, 'is your age')
+# print(type(a))
+
+# # 여러가지 출력방식
+# a = "python"
+# print("%s, Hello~"%a)
+
+name = 'hong gil dong'
+age = 20.5
+print("name is {}, age is {}".format(name, age))
